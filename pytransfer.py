e6051d707eba98881c5e62bb9502ae1ecf6abc37
natoe33 <natoe33@gmail.com>
2014-03-31 00:19:30 -0400

    Added a little more

--- pytransfer.py
+++ pytransfer.py
@@ -1,13 +1,17 @@
 import os
-import glob
 import shutil
 
 newpath = r'<path>'
 src = r'<path>'
 results = []
+paths = ['.mp4', '.avi', '.mkv'] #common video file extentions
 
 if not os.path.exists(newpath): os.makedirs(newpath)
 
-results += glob.glob(src + '.css')
+while true
+	#this will eventually iterate over a few different file extensions
+	results += [each for each in os.listdir(src) if each.endswith('<file extention type>')]
 
-print results
+	for i in results:
+    	if os.path.getsize(src + '\\' + i) > 10: #arbitrary file size
+        	shutil.move((src + '\\' + i), newpath)
