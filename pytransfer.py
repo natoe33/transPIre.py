6a62a37e447c3738f6e6225846aa29df83350387
natoe33 <natoe33@gmail.com>
2014-03-30 22:55:08 -0400

    Removed my personal path from the code

--- pytransfer.py
+++ pytransfer.py
@@ -2,8 +2,8 @@ import os
 import glob
 import shutil
 
-newpath = r'C:\Users\Nate\Downloads\new'
-src = r'C:\Users\Nate\Downloads\test'
+newpath = r'<path>'
+src = r'<path>'
 results = []
 
 if not os.path.exists(newpath): os.makedirs(newpath)
